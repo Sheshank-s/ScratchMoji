231f0ea8129120518f2faa2031e011c9b5f81a99
Sheshank Shankar <sheshank@outlook.com>
2017-11-11 12:33:51 -0800

    d

--- extensions/code.js
+++ extensions/code.js
@@ -1,1 +1,7 @@
-
+let emojis = {
+    ":D": '<img src="assets/emojis/faceemoji/grinning.png" alt="_:D_" title="_:D_" class="emoji">',
+    ":|": '<img src="assets/emojis/faceemoji/neutral-face.png" alt="_:|_" title="_:|_" class="emoji">',
+    ":P": '<img src="assets/emojis/faceemoji/stuck-out-toungue" alt="_:P_" title="_:P_" class="emoji">',
+    "xD": '<img src="https://scratchtools.tk/isonline/assets/absent.svg" alt="_xD_" title="_xD_" class="emoji">',
+    "xP": '<img src="https://scratchtools.tk/isonline/isonline-logo.png" alt="_xP_" title="_xP_" class="emoji">'
+};
