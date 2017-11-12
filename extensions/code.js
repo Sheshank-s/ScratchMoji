84f0aa56e6824b230abebd99f5cb232fdffeeb9b
Sheshank Shankar <sheshank@outlook.com>
2017-11-11 20:07:08 -0800

    m

--- extensions/code.js
+++ extensions/code.js
@@ -1,7 +1,8 @@
 var emojis = {
-    ":D": '<img src=window.location.replace(chrome.extension.getURL("assets/emojis/faceemoji/grinning.png")) alt="_:D_" title="_:D_" class="emoji">',
-    ":P": '<img src=window.location.replace(chrome.extension.getURL("assets/emojis/faceemoji/stuck-out-toungue.png")) alt="_:P_" title="_:P_" class="emoji">',
-    "xD": '<img src=window.location.replace(chrome.extension.getURL("assets/emojis/faceemoji/laughing.png")) alt="_xD_" title="_xD_" class="emoji">',
-    "xP": '<img src=window.location.replace(chrome.extension.getURL("assets/emojis/faceemoji/stuck-out-toungue-winking-eyes.png")) alt="_xP_" title="_xP_" class="emoji">'
+    ":D": '<img style="height: 24px; width: 24px;" src="https://greenbayrules.github.io/host/emojis/faceemoji/grinning.png" alt="_:D_" title="_:D_" class="emoji">',
+    ":P": '<img style="height: 24px; width: 24px;" src="https://greenbayrules.github.io/host/emojis/faceemoji/stuck-out-toungue.png" alt="_:P_" title="_:P_" class="emoji">',
+    "xD": '<img style="height: 24px; width: 24px;" src="https://greenbayrules.github.io/host/emojis/faceemoji/laughing.png" alt="_xD_" title="_xD_" class="emoji">',
+    "xP": '<img style="height: 24px; width: 24px;" src="https://greenbayrules.github.io/host/emojis/faceemoji/stuck-out-toungue-winking-eyes.png" alt="_xP_" title="_xP_" class="emoji">'
 };
+Array.from(comments.querySelectorAll(".comment > .info > .content")).forEach(comment => Object.keys(emojis).forEach(emoji => comment.innerHTML = comment.innerHTML.replace(new RegExp("(\\s|^)" + emoji, "g"), `$1${emojis[emoji]}`)));
 Array.from(comments.querySelectorAll(".comment > .info > .content")).forEach(comment => Object.keys(emojis).forEach(emoji => comment.innerHTML = comment.innerHTML.replace(new RegExp("(\\s|^)_" + emoji + "_", "g"), `$1${emojis[emoji]}`)));
