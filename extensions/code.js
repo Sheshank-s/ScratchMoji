654c0430b3a4f4cdff0d3254de0b1b22f3ce158b
Sheshank Shankar <sheshank@outlook.com>
2017-11-12 21:00:42 -0800

    d

--- extensions/code.js
+++ extensions/code.js
@@ -260,14 +260,10 @@ function handleEmojis() {
 var x = setInterval(function(){
   handleEmojis();
 }, 30);
-        if (document.getElementsByClassName("overview")[0].innerHTML.toLowerCase().includes("scratchmoji.tk") || document.getElementsByClassName("overview")[1].innerHTML.toLowerCase().includes("scratchmoji.tk")) {
-            document.getElementsByClassName("header-text")[0].getElementsByTagName("h2")[0].innerHTML += ' <a href="https://scratch.mit.edu/users/scratchmoji" target="_blank" title="ScratchMoji User" style="text-decoration: none;"><img style="width:25px; height:25px;" src="http://cdn2.scratch.mit.edu/get_image/user/27260179_60x60.png"></img></a>';
+        if (document.getElementById("bio").innerHTML.toLowerCase().includes("scratchmoji.tk") || document.getElementById("status").innerHTML.toLowerCase().includes("scratchmoji.tk")) {
+            document.getElementsByClassName("header-text")[0].getElementsByTagName("h2")[0].innerHTML += ' <a href="https://scratch.mit.edu/users/scratchmoji" target="_blank" title="ScratchMoji User" style="text-decoration: none;"><img style="width:25px; vertical-align: text-bottom; height:25px;" src="http://cdn2.scratch.mit.edu/get_image/user/27260179_60x60.png"></img></a>';
           }
 
-        if (document.getElementById("bio").innerHTML.toLowerCase().includes("scratchmoji.tk") || document.getElementById("status").innerHTML.toLowerCase().includes("scratchmoji.tk")) {
-            document.getElementsByClassName("header-text")[0].getElementsByTagName("h2")[0].innerHTML += ' <a href="https://scratch.mit.edu/users/scratchmoji" target="_blank" title="ScratchMoji User" style="text-decoration: none;"><img style="width:25px; height:25px;" src="http://cdn2.scratch.mit.edu/get_image/user/27260179_60x60.png"></img></a>';
-          }
-        
 
 
   var domChange = function(records){
