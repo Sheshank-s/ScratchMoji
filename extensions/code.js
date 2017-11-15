af2abc0c6a605fd43dd1db85bba8a84616389773
Sheshank Shankar <sheshank@outlook.com>
2017-11-14 20:09:11 -0800

    add

--- extensions/code.js
+++ extensions/code.js
@@ -245,10 +245,14 @@ function handleEmojis() {
     //Potato
     var customemojisdash = {
       "cb": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/2755634_60x60.png" alt="_cb_" title="_cb_" class="emoji">',
+      "dhilly": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/2881193_60x60.png" alt="_dhilly_" title="_dhilly_" class="emoji">',
       "fajtv": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/4291425_60x60.png" alt="_fajtv_" title="_fajtv_" class="emoji">',
+      "fancy": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/13191348_60x60.png" alt="_fancy_" title="_fancy_" class="emoji">',
+      "gam": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/13807747_60x60.png" alt="_gam_" title="_gam_" class="emoji">',
       "jim": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/18324063_60x60.png" alt="_jim_" title="_jim_" class="emoji">',
       "josh": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/16015216_350x350.png" alt="_josh_" title="_josh_" class="emoji">',
       "k11": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="http://i.cubeupload.com/AZi3IX.png" alt="_k11_" title="_k11_" class="emoji">',
+      "p2c": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/49156_350x350.png" alt="_p2c_" title="_p2c_" class="emoji">',
       "potato": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://images.emojiterra.com/emojione/v2/512px/1f954.png" alt="_potato_" title="_potato_" class="emoji">',
       "quirt": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="https://cdn2.scratch.mit.edu/get_image/user/14638446_60x60.png" alt="_quirt_" title="_quirt_" class="emoji">',
       "scratchmoji": '<img style="vertical-align: bottom; display: inline; height: 21px; width: 21px;" src="http://cdn2.scratch.mit.edu/get_image/user/27260179_60x60.png" alt="_scratchmoji_" title="_scratchmoji_" class="emoji">',
